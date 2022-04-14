b85df78bc307140f5742f33106be5c10cbaf0634
Abdullah Janjua <Abdullah.Janjua@dvla.gov.uk>
2022-04-14 18:45:30 +0100

    adding daily hadith

--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import WelcomeMain from '../components/welcomeMain';
 import PrayerTime from '../components/prayerTime';
+import DailyHadith from '../components/dailyHadith';
 
 import '../styles/mainPage.css'
 
@@ -12,6 +13,7 @@ class MainPage extends Component {
             <div className="main-page-container">
                 <WelcomeMain></WelcomeMain>
                 <PrayerTime></PrayerTime>
+                <DailyHadith></DailyHadith>
             </div>
         )
 
