2d366cf0d8247c69c9dad58a563c5daf7ae776c0
Abdullah Janjua <AbdullahJanjuauk@gmail.com>
2022-05-13 00:28:23 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Footer from "./components/footer";
 import AboutPage from "./pages/aboutPage";
-import { useEffect, useState } from "react";
+
 
 function App() {
   return (
